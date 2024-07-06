dfb72401910679fef1295a914892f09390568167
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-05 21:56:34 -0300

    feat: creating entities

---
+++ src/main/java/com/abreu/btgpactual/orderms/models/OrderEntity.java
@@ -0,0 +1,33 @@
+package com.abreu.btgpactual.orderms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document(collection = "tb_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEntity {
+
+    @MongoId
+    private Long orderId;
+
+    @Indexed(name = "customer_id_index")
+    private Long customerId;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal total;
+
+    private List<OrderItem> items;
+}
---
+++ src/main/java/com/abreu/btgpactual/orderms/models/OrderItem.java
@@ -0,0 +1,24 @@
+package com.abreu.btgpactual.orderms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    private String product;
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+
+}
