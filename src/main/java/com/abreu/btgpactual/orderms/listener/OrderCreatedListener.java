062f477464f037b1135656133a10a3916ca86154
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-07 18:40:09 -0300

    feat: updating listener to register order in the system

--- src/main/java/com/abreu/btgpactual/orderms/listener/OrderCreatedListener.java
+++ src/main/java/com/abreu/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -1,6 +1,7 @@
 package com.abreu.btgpactual.orderms.listener;
 
 import com.abreu.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import com.abreu.btgpactual.orderms.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
@@ -12,9 +13,17 @@ import static com.abreu.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_Q
 @Slf4j
 public class OrderCreatedListener {
 
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
         log.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
     }
 
 }
